043fbf9ab7de0aa5ce16fed26cf17cc0f38dd7dd
MilenaGalvão <milenasg91@gmail.com>
2022-09-02 21:49:31 -0300

    Exercicio pra trocar de texto de acordo com o botão clicado.

--- aula04/src/app/app.module.ts
+++ aula04/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ExEventoComponent } from './views/ex-evento/ex-evento.component';
 import { Ex05Component } from './views/ex05/ex05.component';
+import { Ex06Component } from './views/ex06/ex06.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExEventoComponent,
-    Ex05Component
+    Ex05Component,
+    Ex06Component
   ],
   imports: [
     BrowserModule
---
+++ aula04/src/app/views/ex06/ex06.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ex06',
+  templateUrl: './ex06.component.html',
+  styleUrls: ['./ex06.component.css']
+})
+export class Ex06Component implements OnInit {
+
+  texto = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  trocarTexto(palavra: string) {
+    this.texto = palavra;
+
+     }
+  }
+
+
