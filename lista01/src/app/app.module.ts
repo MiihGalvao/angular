23dba0ce3b481b5984c957e020497b05ebd887d0
MilenaGalvão <milenasg91@gmail.com>
2022-09-09 20:42:03 -0300

    Instalação do projeto novo Lista 01
    Resolução da lista01 de Exercícios.

---
+++ lista01/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Ex01Component } from './views/ex01/ex01.component';
+import { Ex02Component } from './views/ex02/ex02.component';
+import { Ex03Component } from './views/ex03/ex03.component';
+import { Ex04Component } from './views/ex04/ex04.component';
+import { Ex05Component } from './views/ex05/ex05.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Ex01Component,
+    Ex02Component,
+    Ex03Component,
+    Ex04Component,
+    Ex05Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
