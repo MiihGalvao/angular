2d3f18d7f0101e6043ab0c6885598a53a9b4e63b
MilenaGalvão <milenasg91@gmail.com>
2022-09-09 22:00:59 -0300

    Ex10 - Calculadora

--- lista01/src/app/views/ex10/ex10.component.ts
+++ lista01/src/app/views/ex10/ex10.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Ex10Component implements OnInit {
 
+  valor1 = '';
+  valor2 = '';
+  resultado = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  atribuirValor1(valor: string) {
+    this.valor1 += valor;
+  }
+
+  atribuirValor2(valor: string) {
+    this.valor2 += valor;
+  }
+
+  somar() {
+    this.resultado = Number(this.valor1) + Number(this.valor2);
+  }
+  subtrair() {
+    this.resultado = Number(this.valor1) - Number(this.valor2);
+  }
+
+  mult() {
+    this.resultado = Number(this.valor1) * Number(this.valor2);
+  }
+
+  div() {
+    this.resultado = Number(this.valor1) / Number(this.valor2);
+  }
 }
