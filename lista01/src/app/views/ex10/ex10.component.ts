280f947495eac1c293084c795990c0e8779223a1
MilenaGalvão <milenasg91@gmail.com>
2022-09-09 21:43:53 -0300

    Alteração do gray (color).

---
+++ lista01/src/app/views/ex10/ex10.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ex10',
+  templateUrl: './ex10.component.html',
+  styleUrls: ['./ex10.component.css']
+})
+export class Ex10Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
