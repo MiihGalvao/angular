f8e031a586f722854830bdbc5e2f4ddfc0ea94e1
MilenaGalvão <milenasg91@gmail.com>
2022-09-09 22:01:21 -0300

    Chamada de exercicios no app component

--- lista01/src/app/app.module.ts
+++ lista01/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { Ex02Component } from './views/ex02/ex02.component';
 import { Ex03Component } from './views/ex03/ex03.component';
 import { Ex04Component } from './views/ex04/ex04.component';
 import { Ex05Component } from './views/ex05/ex05.component';
+import { Ex06Component } from './views/ex06/ex06.component';
+import { Ex07Component } from './views/ex07/ex07.component';
+import { Ex08Component } from './views/ex08/ex08.component';
+import { Ex09Component } from './views/ex09/ex09.component';
+import { Ex10Component } from './views/ex10/ex10.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +20,12 @@ import { Ex05Component } from './views/ex05/ex05.component';
     Ex02Component,
     Ex03Component,
     Ex04Component,
-    Ex05Component
+    Ex05Component,
+    Ex06Component,
+    Ex07Component,
+    Ex08Component,
+    Ex09Component,
+    Ex10Component
   ],
   imports: [
     BrowserModule
