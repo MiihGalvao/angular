3322c1fabb927838f75c7b4fda4c5776d8178e8c
MilenaGalvão <milenasg91@gmail.com>
2022-09-16 22:08:38 -0300

    Exercicios para treino- todos especificados de acordo com numero do ex.

--- aula06/src/app/app.module.ts
+++ aula06/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DuasViasComponent } from './views/duas-vias/duas-vias.component';
+import { Ex01Component } from './views/ex01/ex01.component';
+import { Ex02Component } from './views/ex02/ex02.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DuasViasComponent
+    DuasViasComponent,
+    Ex01Component,
+    Ex02Component
   ],
   imports: [
     BrowserModule,
---
+++ aula06/src/app/views/duas-vias/duas-vias.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-duas-vias',
+  templateUrl: './duas-vias.component.html',
+  styleUrls: ['./duas-vias.component.css']
+})
+export class DuasViasComponent implements OnInit {
+
+  nome = '';
+  sobrenome = '';
+
+  valor1 = 0;
+  valor2 = 0;
+
+  color='';
+
+  generoFilme ='';
+
+  contatoTelefone = false;
+  contatoEmail = false;
+
+  classeHeroi = '';
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  limparNome(){
+    this.nome = '';
+  }
+
+  obterNomeCompleto(){
+    return (this.nome + this.sobrenome).toUpperCase();
+  }
+
+
+}
+
+
