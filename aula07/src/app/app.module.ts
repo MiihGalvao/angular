4c7095914c3d91c06486ad6587b994d65bcdd956
MilenaGalvão <milenasg91@gmail.com>
2022-09-23 22:09:58 -0300

    Conf iniciais dos projetos

--- aula07/src/app/app.module.ts
+++ aula07/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExemploNgifComponent } from './views/exemplo-ngif/exemplo-ngif.component';
+import { Ex01NgifComponent } from './views/ex01-ngif/ex01-ngif.component';
+import { Ex02NgifComponent } from './views/ex02-ngif/ex02-ngif.component';
+import { Ex03NgifComponent } from './views/ex03-ngif/ex03-ngif.component';
+import { ExemploNgforComponent } from './views/exemplo-ngfor/exemplo-ngfor.component';
+import { Ex01NgForComponent } from './views/ex01-ng-for/ex01-ng-for.component';
+import { Ex02NgForComponent } from './views/ex02-ng-for/ex02-ng-for.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExemploNgifComponent
+    ExemploNgifComponent,
+    Ex01NgifComponent,
+    Ex02NgifComponent,
+    Ex03NgifComponent,
+    ExemploNgforComponent,
+    Ex01NgForComponent,
+    Ex02NgForComponent
   ],
   imports: [
     BrowserModule,
