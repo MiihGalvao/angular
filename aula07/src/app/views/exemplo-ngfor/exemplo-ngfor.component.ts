5f58ff363b4dd241905fbfb32de96f9cac3f2054
MilenaGalvão <milenasg91@gmail.com>
2022-09-23 21:51:24 -0300

    Declara os filmes em um array e exibe em uma tabela
    Declara os filmes em um array e exibe em uma tabela - A tabela exibe filme e nota

---
+++ aula07/src/app/views/exemplo-ngfor/exemplo-ngfor.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-ngfor',
+  templateUrl: './exemplo-ngfor.component.html',
+  styleUrls: ['./exemplo-ngfor.component.css']
+})
+export class ExemploNgforComponent implements OnInit {
+
+  animais = ['gato','cachorro','calopsita','onça'];
+
+  filmes =['Titanic', 'A Orfã', 'Senhor dos Anéis'];
+  notas = [9,8,10];
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
