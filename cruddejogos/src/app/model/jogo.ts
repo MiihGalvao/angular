f537d9497c4f7e2b24398c9a0c08f5e00c0f7264
MilenaGalvão <milenasg91@gmail.com>
2022-10-07 21:56:37 -0300

    CRUD Jogos - Incompleto

---
+++ cruddejogos/src/app/model/jogo.ts
@@ -0,0 +1,7 @@
+export class Jogo
+{
+  nomeJogo = '';
+  produtora = '';
+  plataforma= '';
+  genero = '';
+}
--- cruddejogos/src/app/views/jogos/jogos.component.ts
+++ cruddejogos/src/app/views/jogos/jogos.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogosComponent implements OnInit {
 
-  constructor() { }
+  //jogos: Jogo[] = []
+  jogos = new Array <Jogo>();
+  jogoSelecionado?: Jogo
+
+  constructor() {
+    const JogoGta = new Jogo();
+    JogoGta.nome = 'GTA San Andreas';
+    JogoGta.produtora = 'RockStar';
+    JogoGta.plataforma = 'Playstation 2';
+    JogoGta.genero = '';
+
+    this.jogos.push(JogoGta);
+  }
 
   ngOnInit(): void {
+  }
+
+  selecionarJogo(jogo: Jogo) {
+    this.jogoSelecionado = jogo;
+  }
+
+  fechar() {
+    this.jogoSelecionado = undefined;
+  }
+
+  novoJogo() {
+    this.jogoSelecionado = new Jogo();
+    this.jogo.push(this.jogoSelecionado);
+  }
+
+  removerJogo(index: number){
+    this.jogos.splice(index, 1);
+    this.fechar();
   }
 
 }
