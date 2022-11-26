e9460c04f2d3224d145428410a7f0fd507077fba
MilenaGalvão <milenasg91@gmail.com>
2022-11-25 22:03:22 -0300

    atualização de itens angular material

--- crud-rest/src/app/app.module.ts
+++ crud-rest/src/app/app.module.ts
@@ -6,9 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { ClienteComponent } from './views/cliente/cliente.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -26,7 +33,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     MatIconModule,
     MatSidenavModule,
     BrowserAnimationsModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
